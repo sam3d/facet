61e75c638ff02c56589afcd934b41de907060e61
Sam Holmes <samholmes1337@gmail.com>
2021-12-21 10:25:06 +0000

    Create initial monaco editor instance

--- docs/src/pages/play.tsx
+++ docs/src/pages/play.tsx
@@ -1,7 +1,39 @@
+import Editor from "@monaco-editor/react";
 import { NextPage } from "next";
+import Link from "next/link";
 
 const PlayPage: NextPage = () => {
-  return <div>Play page</div>;
+  return (
+    <div className="flex fixed inset-0 flex-col bg-gray-900">
+      <Navbar />
+
+      <div className="flex flex-1">
+        <div className="flex-1">
+          <Editor />
+        </div>
+
+        <div className="flex-1"></div>
+      </div>
+    </div>
+  );
 };
 
 export default PlayPage;
+
+function Navbar() {
+  return (
+    <div className="p-4 text-gray-100">
+      <div className="flex items-center">
+        <Link href="/">
+          <a className="text-gray-500 hover:text-gray-300">&larr;</a>
+        </Link>
+
+        <p className="ml-2 text-xl font-medium">facet</p>
+
+        <p className="py-1 px-2 ml-2 text-xs font-medium tracking-wider uppercase bg-indigo-600 rounded">
+          play
+        </p>
+      </div>
+    </div>
+  );
+}
